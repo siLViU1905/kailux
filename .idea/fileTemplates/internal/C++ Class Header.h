0d0b5f77875f474fe16b7130cfd2ad15d4aaed5e
SiLViU <silviuptc@gmail.com>
2026-03-14 12:49:22 +0200

    Base engine structure

---
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace kailux {
+
+class ${NAME} {
+public:
+
+};
+
+}
---
+++ conanfile.py
@@ -0,0 +1,32 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+from conan.tools.files import copy
+import os
+
+class SLinkRecipe(ConanFile):
+    name = "kailux"
+    version = "1.0"
+    package_type = "application"
+    settings = "os", "compiler", "build_type", "arch"
+
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("glfw/3.4")
+        # self.requires("imgui/1.92.5")
+        # self.requires("stb/cci.20240531")
+
+    # def configure(self):
+        # self.options["imgui"].with_glfw = True
+        # self.options["imgui"].with_vulkan = True
+
+    def layout(self):
+        cmake_layout(self)
+
+    # def generate(self):
+        # imgui = self.dependencies["imgui"]
+        # bindings_dir = os.path.join(imgui.package_folder, "res", "bindings")
+        # dest_dir = os.path.join(self.source_folder, "engine", "src", "core", "imgui_bindings")
+        #
+        # for pattern in ["*imgui_impl_glfw*", "*imgui_impl_vulkan*"]:
+        #     copy(self, pattern, bindings_dir, dest_dir)
