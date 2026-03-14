d4b019a0e52325e0682ad209cabde0db5c544d09
SiLViU <silviuptc@gmail.com>
2026-03-14 13:35:20 +0200

    Add Window abstraction with GLFW integration

---
+++ engine/src/core/Window.cpp
@@ -0,0 +1,138 @@
+#include "Window.h"
+
+#include <stdexcept>
+
+namespace kailux
+{
+    Window::Window() : m_WindowHandle(nullptr), m_Width(0), m_Height(0), m_FramebufferResized(false)
+    {
+    }
+
+    GLFWwindow * Window::getGLFWWindow()
+    {
+        return m_WindowHandle;
+    }
+
+    Window::Window(GLFWwindow *handle, int width, int height)
+        : m_WindowHandle(handle), m_Width(width), m_Height(height), m_FramebufferResized(false)
+    {
+    }
+
+    Window Window::create(int width, int height, std::string_view title)
+    {
+        if (!glfwInit())
+            throw std::runtime_error("Failed to init GLFW");
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        GLFWwindow *handle = glfwCreateWindow(width, height, title.data(), nullptr, nullptr);
+        if (!handle)
+        {
+            glfwTerminate();
+            throw std::runtime_error("Failed to create GLFW window");
+        }
+
+        Window window(handle, width, height);
+
+        glfwSetFramebufferSizeCallback(handle, glfw_framebufferCallback);
+
+        return window;
+    }
+
+    void Window::updateUserPointer()
+    {
+        glfwSetWindowUserPointer(m_WindowHandle, this);
+    }
+
+    Window::Window(Window &&other) noexcept : m_WindowHandle(other.m_WindowHandle),
+                                              m_FramebufferResized(other.m_FramebufferResized),
+                                              m_Width(other.m_Width),
+                                              m_Height(other.m_Height)
+    {
+        other.m_WindowHandle = nullptr;
+
+        other.m_FramebufferResized = false;
+
+        other.m_Width = 0;
+
+        other.m_Height = 0;
+    }
+
+    bool Window::isOpen() const
+    {
+        return !glfwWindowShouldClose(m_WindowHandle);
+    }
+
+    void Window::close()
+    {
+        glfwSetWindowShouldClose(m_WindowHandle, true);
+    }
+
+    bool Window::wasResized()
+    {
+        bool temp = m_FramebufferResized;
+
+        m_FramebufferResized = false;
+
+        return temp;
+    }
+
+    void Window::maximize()
+    {
+        glfwMaximizeWindow(m_WindowHandle);
+    }
+
+    bool Window::isMaximized() const
+    {
+        return static_cast<bool>(glfwGetWindowAttrib(m_WindowHandle, GLFW_MAXIMIZED));
+    }
+
+    bool Window::isMinimized() const
+    {
+        return !m_Width || !m_Height;
+    }
+
+    void Window::restore()
+    {
+        glfwRestoreWindow(m_WindowHandle);
+    }
+
+    void Window::resize(int width, int height)
+    {
+        glfwSetWindowSize(m_WindowHandle, width, height);
+    }
+
+    void Window::getFramebufferSize(int &width, int &height) const
+    {
+        glfwGetFramebufferSize(m_WindowHandle, &width, &height);
+    }
+
+    void Window::pollEvents() const
+    {
+        glfwPollEvents();
+    }
+
+    void Window::waitForEvents() const
+    {
+        glfwWaitEvents();
+    }
+
+    void Window::glfw_framebufferCallback(GLFWwindow *window, int width, int height)
+    {
+        auto *self = static_cast<Window *>(glfwGetWindowUserPointer(window));
+
+        self->m_FramebufferResized = true;
+
+        self->m_Width = width;
+
+        self->m_Height = height;
+    }
+
+    Window::~Window()
+    {
+        glfwDestroyWindow(m_WindowHandle);
+
+        glfwTerminate();
+    }
+}
---
+++ engine/src/core/Window.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <string_view>
+#include <GLFW/glfw3.h>
+
+namespace kailux
+{
+    class Window
+    {
+    public:
+        Window();
+        Window(const Window&) = delete;
+        Window& operator=(const Window&) = delete;
+        Window(Window&& other) noexcept;
+        Window& operator=(Window&& other) noexcept;
+        ~Window();
+
+        static Window create(int width, int height, std::string_view title);
+        void updateUserPointer();
+
+        bool isOpen() const;
+        void close();
+        void pollEvents() const;
+        void waitForEvents() const;
+
+        bool wasResized();
+        void maximize();
+        void restore();
+        void resize(int width, int height);
+
+        bool isMaximized() const;
+        bool isMinimized() const;
+
+        void getFramebufferSize(int& width, int& height) const;
+        constexpr int getWidth()  const { return m_Width;  }
+        constexpr int getHeight() const { return m_Height; }
+
+        GLFWwindow* getGLFWWindow();
+
+    private:
+        explicit Window(GLFWwindow* handle, int width, int height);
+
+        static void glfw_framebufferCallback(GLFWwindow* window, int width, int height);
+
+        GLFWwindow* m_WindowHandle;
+        int         m_Width;
+        int         m_Height;
+        bool        m_FramebufferResized;
+    };
+}
