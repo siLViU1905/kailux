ff07e29c9371390d086d9e2336f3c3ffefb2bbd7
SiLViU <silviuptc@gmail.com>
2026-03-20 19:18:32 +0200

    Add basic shader support and GLSL compiler script

---
+++ engine/src/core/shaders/vertex_shader.glsl
@@ -0,0 +1,11 @@
+#version 460
+
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aNormal;
+layout(location = 2) in vec2 aUv;
+layout(location = 3) in vec4 aTangent;
+
+void main()
+{
+    gl_Position = vec4(aPos, 1.0);
+}
