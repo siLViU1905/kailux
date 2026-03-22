522c1d3e27aaccbb48f2c32b88dc1b2ffe45756f
SiLViU <silviuptc@gmail.com>
2026-03-22 15:48:12 +0200

    Implemented Camera class

---
+++ engine/src/core/Camera.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <glm/glm.hpp>
+
+namespace kailux
+{
+    class Camera
+    {
+    public:
+        Camera();
+
+        static Camera create(int windowWidth, int windowHeight, glm::vec3 position, float speed = 2.f, float sensitivity = 1.f);
+
+        //using raw key codes until proper key handling implemented
+        void updateMovement(int key, float deltaTime);
+        void updateLookAt(double mousePosX, double mousePosY, float deltaTime);
+        void updateProjection(int width, int height);
+
+        void           gainFocus();
+        void           loseFocus();
+        constexpr bool isFocused() const {return m_Focused;}
+
+        constexpr const glm::vec3 &getPosition() const { return m_Position; }
+        void                       setSensitivity(float sensitivity);
+        constexpr float            getSensitivity() const {return m_Sensitivity;}
+        void                       setPitch(float pitch);
+        constexpr float            getPitch() const {return m_Pitch;}
+
+    private:
+        void updatePitch();
+
+        glm::vec3 m_Position;
+        glm::vec3 m_Forward;
+        glm::vec3 m_Right;
+        glm::vec3 m_Up;
+        glm::mat4 m_View;
+        glm::mat4 m_Projection;
+        float     m_Yaw;
+        float     m_Pitch;
+        float     m_Speed;
+        double    m_LastMousePosX;
+        double    m_LastMousePosY;
+        bool      m_Focused;
+        float     m_Sensitivity;
+    };
+}
