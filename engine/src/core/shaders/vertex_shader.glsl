2a7e886c4aca65bbbc3cd8f5ce56ecc24c8d9ffb
SiLViU <silviuptc@gmail.com>
2026-03-27 23:49:26 +0200

    Added extension for gl_DrawID and color meshes based on their culled faces

--- engine/src/core/shaders/vertex_shader.glsl
+++ engine/src/core/shaders/vertex_shader.glsl
@@ -1,9 +1,13 @@
 #version 460
+#extension GL_ARB_shader_draw_parameters : enable
 
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aNormal;
 layout(location = 2) in vec2 aUv;
 layout(location = 3) in vec4 aTangent;
+
+layout(location = 0) out vec3 fragPos;
+layout(location = 1) out vec3 fragNormal;
 
 layout(binding = 0) uniform Camera
 {
@@ -15,5 +19,10 @@ camera;
 
 void main()
 {
-    gl_Position = camera.projection * camera.view * vec4(aPos, 1.0);
+    fragPos = aPos;
+    fragNormal = aNormal;
+
+    vec3 modifiedPos = aPos;
+    modifiedPos.x += float(gl_DrawID) * 1.5;
+    gl_Position = camera.projection * camera.view * vec4(modifiedPos, 1.0);
 }
