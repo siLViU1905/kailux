e69ef84ed9f89539515768c07fe01c212962ce53
SiLViU <silviuptc@gmail.com>
2026-04-13 11:41:59 +0300

    Set default title for `FileDialog::open` to "Choose a file".

--- engine/src/core/FileDialog.h
+++ engine/src/core/FileDialog.h
@@ -6,7 +6,8 @@ namespace kailux
     class FileDialog
     {
     public:
+        static constexpr std::string_view s_DefaultTitle = "Choose a file";
         using DialogResult = std::optional<std::vector<std::string>>;
-        static DialogResult open(std::string_view title);
+        static DialogResult open(std::string_view title = s_DefaultTitle);
     };
 }
