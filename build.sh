9525ff26487406c0ccc0ba8f328788d41d0ee3a3
siLViU1905 <168079557+siLViU1905@users.noreply.github.com>
2026-04-15 20:19:12 +0300

    Added build script for linux

---
+++ build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+conan export scripts/imguizmo [cite: 2]
+
+if [ -d "build" ]; then
+    rm -rf build [cite: 2]
+fi
+
+echo "[INFO] Conan install: DEBUG..." [cite: 2]
+conan install . --output-folder=build --build=missing \
+    -s build_type=Debug \
+    -s compiler.cppstd=23 \
+    -c tools.system.package_manager:mode=install \
+    -c tools.system.package_manager:sudo=True
+
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Conan Debug install failed"
+    exit 1
+fi
+
+mkdir -p build/debug
+cd build/debug
+
+cmake ../.. -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_TOOLCHAIN_FILE="../build/Debug/generators/conan_toolchain.cmake" \
+    -DCMAKE_CXX_STANDARD=23 \
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON
+
+if [ $? -eq 0 ]; then
+    echo "[SUCCESS] Debug config ready! Building..."
+    make -j$(nproc)
+else
+    echo "[ERROR] Debug CMake config failed"
+    exit 1
+fi
+
+cd ../..
+
+echo "[INFO] Conan install: RELEASE..."
+conan install . --output-folder=build --build=missing \
+    -s build_type=Release \
+    -s compiler.cppstd=23 \
+    -c tools.system.package_manager:mode=install \
+    -c tools.system.package_manager:sudo=True
+
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Conan Release install failed"
+    exit 1
+fi
+
+mkdir -p build/release
+cd build/release
+
+cmake ../.. -G "Unix Makefiles" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_TOOLCHAIN_FILE="../build/Release/generators/conan_toolchain.cmake" \
+    -DCMAKE_CXX_STANDARD=23 \
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON
+
+if [ $? -eq 0 ]; then
+    echo "[SUCCESS] Release config ready! Building..."
+    make -j$(nproc)
+else
+    echo "[ERROR] Release CMake config failed"
+    exit 1
+fi
