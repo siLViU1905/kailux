0178add487f74ada373726f97996d271c64ddb12
siLViU1905 <168079557+siLViU1905@users.noreply.github.com>
2026-06-14 14:37:42 +0300

    Improvement: Implemented lock free queue

--- engine/src/core/utilities/Queue.h
+++ engine/src/core/utilities/Queue.h
@@ -4,21 +4,18 @@
 
 namespace kailux
 {
-    template<typename T>
+    template<typename T, size_t Capacity = 1024>
     class Queue
     {
     public:
-        Queue() : m_Mutex(create_scoped<std::mutex>())
-        {
-        }
-
-        Queue(const Queue &) = delete;
-
-        Queue &operator=(const Queue &) = delete;
+        Queue() = default;
+        Queue(const Queue&) = delete;
+        Queue& operator=(const Queue&) = delete;
 
         Queue(Queue &&other) noexcept
-            : m_Queue(std::move(other.m_Queue)),
-              m_Mutex(std::move(other.m_Mutex))
+            : m_Buffer(std::move(other.m_Buffer)),
+              m_Head(other.m_Head.load(std::memory_order_relaxed)),
+              m_Tail(other.m_Tail.load(std::memory_order_relaxed))
         {
         }
 
@@ -26,42 +23,52 @@ namespace kailux
         {
             if (this != &other)
             {
-                m_Queue = std::move(other.m_Queue);
-                m_Mutex = std::move(other.m_Mutex);
+                m_Buffer = std::move(other.m_Buffer);
+                m_Head.store(other.m_Head.load(std::memory_order_relaxed),
+                             std::memory_order_relaxed);
+                m_Tail.store(other.m_Tail.load(std::memory_order_relaxed),
+                             std::memory_order_relaxed);
             }
             return *this;
         }
 
         void push(const T& val)
         {
-            std::lock_guard lock(*m_Mutex);
-            m_Queue.push_front(val);
+            auto tail = m_Tail.load(std::memory_order_relaxed);
+            m_Buffer[tail & s_Mask] = val;
+            m_Tail.store(tail + 1, std::memory_order_release);
         }
         void push(T&& val)
         {
-            std::lock_guard lock(*m_Mutex);
-            m_Queue.push_front(std::move(val));
+            auto tail = m_Tail.load(std::memory_order_relaxed);
+            m_Buffer[tail & s_Mask] = std::move(val);
+            m_Tail.store(tail + 1, std::memory_order_release);
         }
         template<typename... Args>
         void emplace(Args&&... args)
         {
-            std::lock_guard lock(*m_Mutex);
-            m_Queue.emplace_front(std::forward<Args>(args)...);
+            auto tail = m_Tail.load(std::memory_order_relaxed);
+            m_Buffer[tail & s_Mask] = T(std::forward<Args>(args)...);
+            m_Tail.store(tail + 1, std::memory_order_release);
         }
 
         using PopResult = std::optional<T>;
         PopResult tryPop()
         {
-            std::lock_guard lock(*m_Mutex);
-            if (m_Queue.empty())
+            auto head = m_Head.load(std::memory_order_relaxed);
+            if (head == m_Tail.load(std::memory_order_acquire))
                 return std::nullopt;
-            auto val = std::move(m_Queue.back());
-            m_Queue.pop_back();
+            auto val = std::move(m_Buffer[head & s_Mask]);
+            m_Head.store(head + 1, std::memory_order_release);
             return val;
         }
 
     private:
-        std::deque<T>          m_Queue;
-        Scoped<std::mutex>     m_Mutex;
+        static constexpr size_t s_Mask = Capacity - 1;
+
+        std::vector<T> m_Buffer{Capacity};
+
+        std::atomic<size_t> m_Head{0};
+        std::atomic<size_t> m_Tail{0};
     };
 }
