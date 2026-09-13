bef63cc16c8093d5140d064bff5957b0acaa8d80
siLViU1905 <168079557+siLViU1905@users.noreply.github.com>
2026-09-13 20:05:34 +0300

    Reduced point shadows resolution

--- engine/src/core/Core.h
+++ engine/src/core/Core.h
@@ -112,7 +112,7 @@ namespace kailux::details
     constexpr uint32_t kShadowCascadeCount{4};
     constexpr uint32_t kShadowMapResolution{2048};
 
-    constexpr uint32_t kPointShadowResolution{1024};
+    constexpr uint32_t kPointShadowResolution{512};
     constexpr uint32_t kPointShadowFaceCount{6};
     constexpr uint32_t kMaxPointShadows{4};
 }
