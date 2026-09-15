972072f53dc96690ad311e97fdc2fff74c0a165f
siLViU1905 <168079557+siLViU1905@users.noreply.github.com>
2026-09-15 18:52:59 +0300

    KLX-41: Adjusted default material params and point light intensity

--- engine/src/core/components/gpu/MeshMaterialData.h
+++ engine/src/core/components/gpu/MeshMaterialData.h
@@ -5,8 +5,8 @@ namespace kailux
 {
     struct MeshMaterialData
     {
-        glm::vec4 albedoAndRoughness{0.75f, 0.75f, 0.75f, 0.5f};
-        glm::vec4 pbrParams{0.5f, 0.5f, 0.f,0.f};
+        glm::vec4 albedoAndRoughness{0.75f, 0.75f, 0.75f, 1.f};
+        glm::vec4 pbrParams{1.f, 0.02f, 0.f,0.f}; // x - metallic y - ao
         uint32_t  materialIdx{};
         std::array<uint32_t, 3> _padding{};
     };
